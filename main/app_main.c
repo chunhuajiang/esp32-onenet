ac0f0fef19c587e8ef5bef6ef77385e71df57088
Tidy Jiang <tidyjiang@163.com>
2017-04-30 11:09:39 +0800

    add initinal source code

---
+++ main/app_main.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "soc/rtc_cntl_reg.h"
+
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event_loop.h"
+
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "config.h"
+#include "debug.h"
+
+#include "mqtt.h"
+
+
+void connected_cb(void *self, void *params)
+{
+    mqtt_client *client = (mqtt_client *)self;
+    mqtt_subscribe(client, "/test", 0);
+    mqtt_publish(client, "/test", "howdy!", 6, 0, 0);
+}
+void disconnected_cb(void *self, void *params)
+{
+
+}
+void reconnect_cb(void *self, void *params)
+{
+
+}
+void subscribe_cb(void *self, void *params)
+{
+    INFO("[APP] Subscribe ok, test publish msg\n");
+    mqtt_client *client = (mqtt_client *)self;
+    mqtt_publish(client, "/test", "abcde", 5, 0, 0);
+}
+
+void publish_cb(void *self, void *params)
+{
+
+}
+void data_cb(void *self, void *params)
+{
+    (void)self;
+    mqtt_event_data_t *event_data = (mqtt_event_data_t *)params;
+
+    if (event_data->data_offset == 0) {
+
+        char *topic = malloc(event_data->topic_length + 1);
+        memcpy(topic, event_data->topic, event_data->topic_length);
+        topic[event_data->topic_length] = 0;
+        INFO("[APP] Publish topic: %s\n", topic);
+        free(topic);
+    }
+
+    // char *data = malloc(event_data->data_length + 1);
+    // memcpy(data, event_data->data, event_data->data_length);
+    // data[event_data->data_length] = 0;
+    INFO("[APP] Publish data[%d/%d bytes]\n",
+         event_data->data_length + event_data->data_offset,
+         event_data->data_total_length);
+         // data);
+
+    // free(data);
+
+}
+
+mqtt_settings settings = {
+    .host = ONENET_HOST,
+    .port = ONENET_PORT,
+    .client_id = ONENET_DEVICE_ID,
+    .username = ONENET_PROJECT_ID,
+    .password = ONENET_AUTH_INFO,
+    .clean_session = 0,
+    .keepalive = 120,
+    .lwt_topic = "/lwt",
+    .lwt_msg = "offline",
+    .lwt_qos = 0,
+    .lwt_retain = 0,
+    .connected_cb = connected_cb,
+    .disconnected_cb = disconnected_cb,
+    .reconnect_cb = reconnect_cb,
+    .subscribe_cb = subscribe_cb,
+    .publish_cb = publish_cb,
+    .data_cb = data_cb
+};
+
+static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
+{
+
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        ESP_ERROR_CHECK(esp_wifi_connect());
+        break;
+
+    case SYSTEM_EVENT_STA_GOT_IP:
+
+        mqtt_start(&settings);
+        // Notice that, all callback will called in mqtt_task
+        // All function publish, subscribe
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* This is a workaround as ESP32 WiFi libs don't currently
+           auto-reassociate. */
+        
+        mqtt_stop();
+        ESP_ERROR_CHECK(esp_wifi_connect());
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+
+
+}
+
+void wifi_conn_init(void)
+{
+    INFO("[APP] Start, connect to Wifi network: %s ..\n", WIFI_SSID);
+
+    tcpip_adapter_init();
+
+    ESP_ERROR_CHECK( esp_event_loop_init(wifi_event_handler, NULL) );
+
+    wifi_init_config_t icfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&icfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS
+        },
+    };
+
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK( esp_wifi_start());
+}
+
+void app_main()
+{
+    INFO("[APP] Startup..\n");
+    INFO("[APP] Free memory: %d bytes\n", esp_get_free_heap_size());
+    INFO("[APP] Build time: %s\n", BUID_TIME);
+
+#ifdef CPU_FREQ_160MHZ
+    INFO("[APP] Setup CPU run as 160MHz\n");
+    SET_PERI_REG_BITS(RTC_CLK_CONF, RTC_CNTL_SOC_CLK_SEL, 0x1, RTC_CNTL_SOC_CLK_SEL_S);
+    WRITE_PERI_REG(CPU_PER_CONF_REG, 0x01);
+    INFO("[APP] Setup CPU run as 160MHz - Done\n");
+#endif
+ 
+    nvs_flash_init();
+    wifi_conn_init();
+}
---
+++ main/include/config.h
@@ -0,0 +1,16 @@
+#ifndef __ONENET_CONFIG_H__
+#define __ONENET_CONFIG_H__
+
+//#define CPU_FREQ_160MHZ
+
+#define WIFI_SSID           "ssid"
+#define WIFI_PASS           "password"
+
+#define ONENET_HOST         ("183.230.40.39")
+#define ONENET_PORT         (6002)
+#define ONENET_DEVICE_ID    ("5619109")                // mqtt client id
+#define ONENET_PROJECT_ID   ("85574" )                 // mqtt username
+#define ONENET_AUTH_INFO    ("tidyjiang-esp32-onenet") // mqtt password
+
+#endif
+
